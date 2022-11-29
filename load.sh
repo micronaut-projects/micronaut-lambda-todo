4895653e39b3d01f2ca93643f2d364a60e338de6
Sergio del Amo <sergio.delamo@softamo.com>
2022-11-29 07:37:55 +0100

    softamodev

--- load.sh
+++ load.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 STACK_NAME=MnTodoStack
 
-export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`JavaApiUrl`].OutputValue' --output text)"
+export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`JavaApiUrl`].OutputValue' --output text --profile=softamodev)"
 echo API_URL
 ./gradlew :loadtests:gatlingRun
 
-export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`NativeApiUrl`].OutputValue' --output text)"
+export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`NativeApiUrl`].OutputValue' --output text --profile=softamodev)"
 ./gradlew :loadtests:gatlingRun
 
-export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`SnapStartApiUrl`].OutputValue' --output text)"
+export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`SnapStartApiUrl`].OutputValue' --output text --profile=softamodev)"
 ./gradlew :loadtests:gatlingRun
 
-export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`SnapStartPrimingApiUrl`].OutputValue' --output text)"
+export API_URL="$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`SnapStartPrimingApiUrl`].OutputValue' --output text --profile=softamodev)"
 ./gradlew :loadtests:gatlingRun
 
--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ if [ $EXIT_STATUS -ne 0 ]; then
  exit $EXIT_STATUS
 fi
 cd infra
-cdk deploy --require-approval never || EXIT_STATUS=$?
+cdk deploy --profile=softamodev --require-approval never || EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
   exit $EXIT_STATUS
 fi
