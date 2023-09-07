4f6fcd9a82f8092e5f46967837d1aee5147c5258
Tim Yates <tim.yates@gmail.com>
2023-09-07 17:10:07 +0100

    Remove gradle test logging, fix spock upload

--- function-java/build.gradle.kts
+++ function-java/build.gradle.kts
@@ -15,9 +15,3 @@ dependencies {
 application {
     mainClass.set("com.example.Application")
 }
-
-tasks.withType<Test> {
-    testLogging {
-        events("passed", "skipped", "failed", "standardOut", "standardError")
-    }
-}
