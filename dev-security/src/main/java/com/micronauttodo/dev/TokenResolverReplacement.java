e1dac37c0de172cb6bb5ccea1da2f26f8b75fd5c
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-25 10:42:16 +0100

    fix TokenResolver replacement

--- dev-security/src/main/java/com/micronauttodo/dev/TokenResolverReplacement.java
+++ dev-security/src/main/java/com/micronauttodo/dev/TokenResolverReplacement.java
@@ -2,11 +2,14 @@ package com.micronauttodo.dev;
 
 import io.micronaut.context.annotation.Replaces;
 import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.security.token.generator.TokenGenerator;
 import io.micronaut.security.token.reader.TokenResolver;
 import jakarta.inject.Singleton;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Requires(beans = DevAuthenticationFetcher.class)
@@ -22,8 +25,17 @@ public class TokenResolverReplacement implements TokenResolver<HttpRequest<?>> {
         this.devAuthenticationFetcher = devAuthenticationFetcher;
     }
 
+
     @Override
+    @Deprecated
     public Optional<String> resolveToken(HttpRequest<?> request) {
         return tokenGenerator.generateToken(devAuthenticationFetcher.claims());
+    }
+
+    @Override
+    public List<String> resolveTokens(HttpRequest<?> request) {
+        return tokenGenerator.generateToken(devAuthenticationFetcher.claims())
+                .map(Collections::singletonList)
+                .orElseGet(Collections::emptyList);
     }
 }
