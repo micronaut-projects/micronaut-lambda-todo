79f2d8ca8e9d856a70828484c41f7e09e7e925f3
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-25 10:37:49 +0100

    update to gradle plugin 4.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.micronaut.library") version "4.0.3" apply false
-    id("io.micronaut.minimal.application") version "4.0.3" apply false
-    id("io.micronaut.application") version "4.0.3" apply false
+    id("io.micronaut.library") version "4.2.0" apply false
+    id("io.micronaut.minimal.application") version "4.2.0" apply false
+    id("io.micronaut.application") version "4.2.0" apply false
 }
